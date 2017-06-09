a2a30d12ed0280d7461be8e936584593a18a8b77
DESKTOP-QG7KIET\BQT <bqtdev@gmail.com>
2017-06-09 15:24:35 +0700

    [Fragment - Dientu] : Add progress bar when loading image

--- app/src/main/java/com/example/bqt/myapp/Adapter/AdapterThuongHieu.java
+++ app/src/main/java/com/example/bqt/myapp/Adapter/AdapterThuongHieu.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.bqt.myapp.Model.ObjectClass.Brand;
@@ -31,21 +33,21 @@ public class AdapterThuongHieu extends RecyclerView.Adapter<AdapterThuongHieu.Vi
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater= (LayoutInflater) myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view=layoutInflater.inflate(R.layout.custom_layout_recycle_thuonghieu,parent,false);
+        LayoutInflater layoutInflater = (LayoutInflater) myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout_recycle_thuonghieu, parent, false);
 
-        ViewHolder viewHolder=new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Brand brand=brands.get(position);
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Brand brand = brands.get(position);
         holder.txt_Tieude_Thuonghieu.setText(brand.getName());
-        Picasso.with(myContext).load("http://192.168.56.1/Itech/UploadFiles/userfiles/images/"+brand.getImage()).resize(120,120).into(holder.img_Thuonghieu, new Callback() {
+        Picasso.with(myContext).load("http://192.168.56.1/Itech/UploadFiles/userfiles/images/" + brand.getImage()).resize(120, 120).into(holder.img_Thuonghieu, new Callback() {
             @Override
             public void onSuccess() {
-
+                holder.progressBar.setVisibility(View.GONE);
             }
 
             @Override
@@ -60,13 +62,16 @@ public class AdapterThuongHieu extends RecyclerView.Adapter<AdapterThuongHieu.Vi
         return brands.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         TextView txt_Tieude_Thuonghieu;
         ImageView img_Thuonghieu;
+        ProgressBar progressBar;
+
         public ViewHolder(View itemView) {
             super(itemView);
-           txt_Tieude_Thuonghieu= (TextView) itemView.findViewById(R.id.txt_tieude);
-            img_Thuonghieu= (ImageView) itemView.findViewById(R.id.img_thuonghieu);
+            txt_Tieude_Thuonghieu = (TextView) itemView.findViewById(R.id.txt_tieude);
+            img_Thuonghieu = (ImageView) itemView.findViewById(R.id.img_thuonghieu);
+            progressBar= (ProgressBar) itemView.findViewById(R.id.progress_bar_download);
         }
     }
 }
